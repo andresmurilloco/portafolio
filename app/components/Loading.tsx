17d87dec0e9814e7e0d2ddc901ca991e84460c20
Andres Murillo Cortazar <dev.andresmurillo@gmail.com>
2023-08-24 12:10:03 -0500

    Minor patch

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
   return (
     <div className='loading-container'>
-        <h2 className='loading-text'>Fetching projects...</h2>
+        <h2 className='loading-text'>Please wait<br/>Fetching projects...</h2>
     </div>
   )
 }
