c2a5bba7fdb3339819e91d7ef7cdef9b87629640
Andres Murillo Cortazar <dev.andresmurillo@gmail.com>
2023-09-09 08:54:01 -0500

    Minor update on API to support English and Spanish settings.

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,9 +2,10 @@
 import { useEffect, useState } from 'react'
 import { getProjects } from '../api/apiconn'
 import Project from './Project'
-import { data } from '../api/apiconn'
+import { data as dataArray } from '../api/apiconn'
 
 const Projects = () => {
+  const data = dataArray[0].english;
 
   return (
       <div className='project-container'>
