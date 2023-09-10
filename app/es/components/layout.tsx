f44c1319bbfd7fa887c92fc8c3dad9bbba73595b
Andres Murillo <dev.andresmurillo@gmail.com>
2023-09-10 09:00:22 -0500

    Metadata update

--- app/es/components/layout.tsx
+++ app/es/components/layout.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import { Footer, Header } from './'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Welcome/Bienvenido',
+    description: 'Portafolio front-den dev Andrés Murillo',
+}
 
 const LayoutEs = ({ children }: any) => {
     return (
