eba3188d8f604e1c63e46f5853576991aead088b
Andrés Murillo <dev.andresmurillo@gmail.com>
2024-10-03 19:40:08 -0500

    Mayor update 4.0.0. Removed almost the entire project. Only main page, header and footers were kept.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import LayoutMain from './components/layout'
-import style from './components/css/maintenance.module.css'
+import './components/css/maintenance.css'
 import maintenanceImage from "@/public/assets/img/maintenance.png"
 import Image from 'next/image'
 export default function Home() {
   
   return (
     <LayoutMain>
-      <div className={style.techContainer}>
-        <Image src={maintenanceImage} alt='In maintenance logo' className={style.maintenanceImage}/>
-        <h1>Lo siento, el portafolio está en proceso de mejora!😊</h1>
+      <div className="techContainer">
+        <Image src={maintenanceImage} alt='In maintenance logo' className="maintenanceImage"/>
+        <h1 className="announcement">Lo siento, el portafolio está en proceso de mejora!😊</h1>
       </div>
     </LayoutMain>
   )
